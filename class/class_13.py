b840e7dae87618eb00f333313a62842c91e8b365
pia222sk20 <pia222.sk20@gmail.com>
2025-09-15 16:40:29 +0900

    property getter setter

--- class/class_13.py
+++ class/class_13.py
@@ -1,28 +1,21 @@
-# 파이썬 클래스에서 getter setter 사용법
+
 import random
 class Person:
     def __init__(self, name, age):
-        self._name = name  # private 변수로 설정
-        self._age = age    # private 변수로 설정
-    # 데코레이터를 이용한 setter
-    @property
-    def name(self):
-        return self._name
-    @name.setter
-    def name(self, name):
-        self._name = name
-
+        self.name = name
+        self._age = age
     @property
     def age(self):
         return self._age
-
+    
     @age.setter
-    def age(self, age):
-        if age < 0:
-            raise ValueError("나이는 음수가 될 수 없습니다.")
-        self._age = age
-p = Person("홍길동", 25)
-print(p.name)
-print(p.age)    
-p.name = "김철수"
-p.age = 30
+    def age(self, value):
+       self._age = value
+
+p1 = Person("홍길동", 20)
+print(p1.age)
+p1.age = 30
+print(p1.age)
+print(p1.name)
+del p1.name
+print(p1.name) #  'Person' object has no attribute 'name'
