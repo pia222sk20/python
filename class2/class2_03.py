daead11b6709cb5e93947888e315c289cfdc6eba
pia222sk20 <pia222.sk20@gmail.com>
2025-09-16 12:13:33 +0900

    다형성

--- class2/class2_03.py
+++ class2/class2_03.py
@@ -27,7 +27,7 @@ class FullTimeEmployee(Employee):
     # bonus도 마이너스 입력 불가        
     def __str__(self):
         return super().__str__() + f', {self.bonus}'
-    def fte(self):
+    def show_class(self):
         print('정규직 클래스')
 # 계약직 PartTimeEmployee- 시간당 급여, 기본급 없음
 class PartTimeEmployee(Employee):
@@ -38,7 +38,7 @@ class PartTimeEmployee(Employee):
     # hourly_rate  hours  : getter setter
     def __str__(self):
         return f'{self.name} {self.id} {self.hourly_rate} {self.hours}'
-    def pte(self):
+    def show_class(self):
         print("계약직 클래스")
 
 # 인턴 Intern - 고정수당
@@ -48,18 +48,30 @@ class Intern(Employee):
         self.fixed_salary = fixed_salary
     def __str__(self):
         return f'{self.name} {self.id} {self.fixed_salary}'
-    def int(self):
+    def show_class(self):
         print('인턴클래스')
 
 # 정규직 직원, 계약직, 인턴을 모두 리스트에 섞어서 객체를 저장
-# emp = [
-#     FullTimeEmployee(),
-#     ...
-#     ...
-#     ...
-#     ...
-#     ..
-# ]
+emp = [
+    FullTimeEmployee('F001', '홍길동', 3000000, 500000),    
+    FullTimeEmployee('F002', '김철수', 2800000, 400000),
+    PartTimeEmployee('P001', '이영희', 20000, 80),
+    PartTimeEmployee('P002', '박민수', 18000, 100),
+    Intern('I001', '최지우', 1000000),
+]
 # emp에 들어있는 직원이 각각 어떤클래인지 순환을 이용해서 각 클래스의 int,pte 메소드 호출
+# for e in emp:
+#     if isinstance(e, FullTimeEmployee):
+#         e.fte()
+#     elif isinstance(e, PartTimeEmployee):
+#         e.pte()
+#     elif isinstance(e, Intern):
+#         e.int()
+#     else:
+#         e.emp()
+
+for e in emp:
+    e.show_class()   # emp리스트의 각각의 객체에 해당하는 메소드를 호출  다형성
+
 
 
