b66a5eda00111b1141d178ad9fbb0e27ea462094
pia222sk20 <pia222.sk20@gmail.com>
2025-09-22 19:35:41 +0900

    .

--- api/01.py
+++ api/01.py
@@ -3,18 +3,13 @@ import os
 import requests
 # .env 로드
 load_dotenv()
-P_KEY = os.getenv('PUBLIC_ENCODE_KEY')
+P_KEY = os.getenv('PUBLIC_DECODE_KEY')
 print(f'P_KEY : {P_KEY[-10:]}')
 # 데이터를 요청 할 주소
 url = 'http://apis.data.go.kr/B553881/newRegistInfoService/newRegistInfoService'
 # 서버에 보낼 데이터(1페이지를 보여달라는 의미로)
 from_data = {
-    'serviceKey' : P_KEY,
-    'registYy' : 2024,
-    'registMt' : 12,
-    'vhctyAsortCode' : 2,
-    'registGrcCode':1,
-    'useFuelCode':6
+    'serviceKey' : P_KEY    
 }
 response = requests.get(url,data=from_data)
 print(response.text[:500])
