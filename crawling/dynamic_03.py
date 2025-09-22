5d26700d2b8c3c5f713f308191ef541ff481f760
pia222sk20 <pia222.sk20@gmail.com>
2025-09-22 16:09:00 +0900

    Add Selenium을 사용한 웹 자동화 스크립트: 웹 드라이버 설치, 사이트 접속 및 라디오 버튼 클릭 기능 구현

---
+++ crawling/dynamic_03.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys  # enter키 등을 입력하기위해서
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup
+import time
+
+url = 'https://auto.danawa.com/auto/?Work=record'
+#웹 드라이버를 자동으로 설치하고 최신버전을 유지
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+# 사이트 접속
+driver.get(url)
+# driver.maximize_window() # 전체 화면으로 실행  옵션
+print('사이트 접속했습니다.')
+# 사이트가 로드될때까지 기다린다.
+time.sleep(3)
+radio =  driver.find_element(By.CSS_SELECTOR,"input[name='rdoMonthPeriod'][value='period']")
+radio.click()
+
+time.sleep(10)
+# 브라우져 종료
+driver.quit()
