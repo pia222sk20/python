746e0e074f57f098adc60074edf53c041c22c7c9
pia222sk20 <pia222.sk20@gmail.com>
2025-09-22 14:33:05 +0900

    Add 웹 드라이버 설정 및 사이트 접속 기능: Selenium을 사용하여 자동으로 웹 드라이버를 설치하고 사이트에 접속하도록 구현

--- crawling/dynamic_02.py
+++ crawling/dynamic_02.py
@@ -1,1 +1,24 @@
-# https://kosis.kr/statisticsList/statisticsListIndex.do?menuId=M_01_01&vwcd=MT_ZTITLE&parmTabId=M_01_01
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys  # enter키 등을 입력하기위해서
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+url = 'https://auto.danawa.com/auto/?Work=record'
+#웹 드라이버를 자동으로 설치하고 최신버전을 유지
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+# 사이트 접속
+driver.get(url)
+driver.maximize_window() # 전체 화면으로 실행
+print('사이트 접속했습니다.')
+# 사이트가 로드될때까지 기다린다.
+
+
+time.sleep(5)
+driver.quit()
