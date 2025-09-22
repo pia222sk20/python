d2852f59c788ae49e3543a52d0aeab936f244598
pia222sk20 <pia222.sk20@gmail.com>
2025-09-22 16:22:53 +0900

    Add 월별 데이터 조회 기능: 조회 버튼 클릭 로직 추가 및 선택된 날짜 범위 설정

--- crawling/dynamic_03.py
+++ crawling/dynamic_03.py
@@ -20,9 +20,27 @@ driver.get(url)
 # driver.maximize_window() # 전체 화면으로 실행  옵션
 print('사이트 접속했습니다.')
 # 사이트가 로드될때까지 기다린다.
-time.sleep(3)
+time.sleep(1)
 radio =  driver.find_element(By.CSS_SELECTOR,"input[name='rdoMonthPeriod'][value='period']")
 radio.click()
+time.sleep(1)
+select = Select(driver.find_element(By.ID,'selMonthFrom'))
+select.select_by_value('2023')
+time.sleep(1)
+select = Select(driver.find_element(By.ID,'selDayFrom'))
+select.select_by_value('01')
+time.sleep(1)
+select = Select(driver.find_element(By.ID,'selMonthTo'))
+select.select_by_value('2023')
+time.sleep(1)
+select = Select(driver.find_element(By.ID,'selDayTo'))
+select.select_by_value('12')
+
+#조회 버튼 클릭  //*[@id="monthPeriodDiv"]/span[2]/input
+# 속성중에 disalble = True 인 엘리먼트는 사용자 클릭을 막아놓음
+btn = driver.find_element(By.XPATH,'//*[@id="monthPeriodDiv"]/span[2]/input')
+driver.execute_script("arguments[0].click()", btn)  # 사용자 마우스 클릭 이벤트를 발생
+# driver.execute_script("selectRecord('period');")  # 직접 실행
 
 time.sleep(10)
 # 브라우져 종료
